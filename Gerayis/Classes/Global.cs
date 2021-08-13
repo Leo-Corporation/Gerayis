7fd8da474e3b4a7a6fbb7d56fd89e36c037cad93
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-08-13 11:39:14 +0200

    Version 1.5.0.2108-rc1

--- Gerayis/Classes/Global.cs
+++ Gerayis/Classes/Global.cs
@@ -41,7 +41,7 @@ namespace Gerayis.Classes
 		/// <summary>
 		/// The current version of Gerayis.
 		/// </summary>
-		public static string Version => "1.5.0.2108-pre1";
+		public static string Version => "1.5.0.2108-rc1";
 
 		/// <summary>
 		/// List of the available languages.
