bf9276f894b93c60deb7f8a261e09b1bb9c349d8
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-09-19 13:39:23 +0200

    Code cleanup

--- Gerayis/Pages/BarCodePage.xaml.cs
+++ Gerayis/Pages/BarCodePage.xaml.cs
@@ -26,19 +26,9 @@ using Gerayis.Enums;
 using Gerayis.UserControls;
 using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Gerayis.Pages
 {
@@ -138,7 +128,7 @@ namespace Gerayis.Pages
 						if (!contains)
 						{
 							BarCodeHistory.Children.Add(new HistoryItem(text, BarCodeHistory, AppPages.BarCode, barcodeType));
-						}					
+						}
 					}
 				}
 				else
--- Gerayis/Pages/QRCodePage.xaml.cs
+++ Gerayis/Pages/QRCodePage.xaml.cs
@@ -26,20 +26,9 @@ using Gerayis.UserControls;
 using Microsoft.Win32;
 using QRCoder;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Gerayis.Pages
 {
@@ -132,7 +121,7 @@ namespace Gerayis.Pages
 						if (!contains)
 						{
 							QRCodeHistory.Children.Add(new HistoryItem(QRCodeStringTxt.Text, QRCodeHistory, Enums.AppPages.QRCode));
-						}					
+						}
 					}
 				}
 				else
