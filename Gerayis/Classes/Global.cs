aa2c6ed8236bb5dd2a82ff636bcbe7c77b2f97a5
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-01-24 07:46:46 +0100

    Added Simplified Chinese language (#100)
    Co-Authored-By: 大眼仔旭 <86864914+wcxu21@users.noreply.github.com>

--- Gerayis/Classes/Global.cs
+++ Gerayis/Classes/Global.cs
@@ -46,12 +46,12 @@ namespace Gerayis.Classes
 		/// <summary>
 		/// List of the available languages.
 		/// </summary>
-		public static List<string> LanguageList => new() { "English (United States)", "Français (France)" };
+		public static List<string> LanguageList => new() { "English (United States)", "Français (France)", "中文（简体）" };
 
 		/// <summary>
 		/// List of the available languages codes.
 		/// </summary>
-		public static List<string> LanguageCodeList => new() { "en-US", "fr-FR" };
+		public static List<string> LanguageCodeList => new() { "en-US", "fr-FR", "zh-CN" };
 
 		/// <summary>
 		/// GitHub link for the last version (<see cref="string"/>).
@@ -166,9 +166,11 @@ namespace Gerayis.Classes
 				case "en-US": // English (US)
 					Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US"); // Change
 					break;
-
 				case "fr-FR": // French (FR)
 					Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("fr-FR"); // Change
+					break;
+				case "zh-CN": // Chinese (CN)
+					Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("zh-CN"); // Change
 					break;
 				default: // No language
 					break;
