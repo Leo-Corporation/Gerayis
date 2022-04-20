78ab7566a491036e7879ab09bf0ce1bf6a722ec6
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-04-20 09:38:12 +0200

    Version 2.1.0.2204-rc1

--- Gerayis/Classes/Global.cs
+++ Gerayis/Classes/Global.cs
@@ -44,7 +44,7 @@ public static class Global
 	/// <summary>
 	/// The current version of Gerayis.
 	/// </summary>
-	public static string Version => "2.1.0.2204-pre1";
+	public static string Version => "2.1.0.2204-rc1";
 
 	/// <summary>
 	/// List of the available languages.
